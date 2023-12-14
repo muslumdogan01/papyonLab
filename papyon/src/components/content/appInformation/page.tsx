d6811cfa47094d9f9ad5d7b81b8708a975f1eb89
muslumdogan01 <muslumdogan01@gmail.com>
2023-12-15 00:44:24 +0300

    line bug fix

--- papyon/src/components/content/appInformation/page.tsx
+++ papyon/src/components/content/appInformation/page.tsx
@@ -1,46 +1,63 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 const AppInformation = () => {
   return (
     <div className="container mx-auto">
       <div className="md:mt-44 mt-[100px] flex w-full justify-center items-center mb-32">
         <div className="relative rounded-[50px] ">
-          <Image
-            src="/appInformation.svg"
-            alt="Logo"
-            width={1200}
-            height={500}
-            id="animatedLine"
-            className=" bg-gradient-to-b rounded-[50px] border-t border-r border-l from-[#21C1F333] to-[#00030F00] -z-10"
-          />
-          {/* <Image
-            src="/informationLine.svg"
-            alt="informationLine"
-            width={1200}
-            height={1}
-            className="absolute top-0 z-0 left-0 hidden lg:block"
-          /> */}
+          <motion.div
+            initial={{ borderColor: "rgba(0, 209, 255, 0.9)" }} // Başlangıç border rengi
+            animate={{ borderColor: "rgba(0, 209, 255, 0)" }} // Bitiş border rengi
+            transition={{
+              duration: 2, // Animasyon süresi
+              repeat: Infinity, // Sonsuz tekrar
+              repeatType: "reverse", // Ters yönde tekrar
+            }}
+            style={{
+              
+              borderStyle: "solid",
+              borderTopColor: "2px",
+              borderRightColor: "1px",
+              borderTopWidth: "2px",
+              borderRightWidth: "1px",
+              borderTopLeftRadius: "50px",
+              borderTopRightRadius: "50px",
+            }}
+          >
+            <Image
+              src="/appInformation.svg"
+              alt="Logo"
+              width={1200}
+              height={500}
+              className=" bg-gradient-to-b  rounded-[50px]  from-[#21C1F333] to-[#00030F00] -z-10"
+            />
+          </motion.div>
 
-          <Image
-            src="/playStore.svg"
-            alt="playStore"
-            width={120}
-            height={120}
-            className="absolute top-[31px] -rotate-[15deg] left-20 hover:-rotate-45 transition duration-1000 ease-linear"
-          />
-          <Image
-            src="/appleStore.svg"
-            alt="playStore"
-            width={120}
-            height={120}
-            className="absolute top-[76px] -rotate-[-15deg] left-[171px] hover:rotate-45 transition duration-1000 ease-linear"
-          />
-          <div className="absolute md:left-[100px] top-[170px] md:right-[58px] flex md:flex-row flex-col">
+          <div className="group ">
+            <Image
+              src="/playStore.svg"
+              alt="playStore"
+              width={120}
+              height={120}
+              className="absolute top-[75px] -rotate-[15deg] left-20 group-hover:-rotate-[-15deg] transition duration-1000 ease-linear"
+            />
+            <Image
+              src="/appleStore.svg"
+              alt="playStore"
+              width={120}
+              height={120}
+              className="absolute top-[120px] -rotate-[-15deg] left-[171px] group-hover:-rotate-[15deg]  transition duration-1000 ease-linear"
+            />
+          </div>
+
+          <div className="absolute md:left-[100px] top-[214px] md:right-[58px] flex md:flex-row flex-col">
             <div className="flex w-full flex-col lg:flex-row">
               <div className="flex flex-col w-full lg:w-1/2">
                 <h1 className="text-[40px] w-full bg-clip-text px-6 bg-gradient-to-r from-[#C583D4] to-[#96B8F8] text-transparent font-semibold leading-[49px] tracking-[-3px]">
-                  Our mobile new world.  We do fun things in 
-                  this world
+                  Our mobile new world. We do fun things in this world
                 </h1>
               </div>
               <div className="flex flex-col w-full lg:w-1/2 mt-7 md:mt-0">
