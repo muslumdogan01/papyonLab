e2561b21e80f37b649928a98c7b8b5f85214513c
muslumdogan01 <muslumdogan01@gmail.com>
2023-12-15 15:36:34 +0300

    delete node

--- papyon/src/app/about/page.tsx
+++ papyon/src/app/about/page.tsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <>
       <Header />
-      <div className="h-24"></div>
+      <div className="h-24 md:h-0"></div>
       <div className="h-full w-full flex flex-col bg-red-300">
         <div className="bg-red-500">selam13333</div>
         <div className="bg-red-500">selam1</div>
